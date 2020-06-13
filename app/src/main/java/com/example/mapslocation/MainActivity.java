a0e0c32667f6f5c08ed391887befa4157d02702f
hoacomay1992 <levanhauvtn@gmail.com>
2020-06-14 00:29:24 +0700

    Change in Activity variable, and init class Student

--- app/src/main/java/com/example/mapslocation/MainActivity.java
+++ app/src/main/java/com/example/mapslocation/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Mainactivity";
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/mapslocation/Student.java
@@ -0,0 +1,4 @@
+package com.example.mapslocation;
+
+public class Student {
+}
